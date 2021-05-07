932a95499d33c5b83c99e47bff7e629ad574a9a8
n <n@noreply.codeberg.org>
2021-05-07 16:27:24 +0900

    Update Font Awesome to v5.15.3

--- font_buildscripts/fontawesome5.sh
+++ font_buildscripts/fontawesome5.sh
@@ -1,9 +1,6 @@
-FA_SUBDIR=fontawesome-free-5.14.0-web
+FA_SUBDIR=fontawesome-free-5.15.3-web
 
 cd $FA_SUBDIR
-rm sprites/brands.svg
-rm -r svgs/brands
-rm webfonts/fa-brands*
 cd ..
 mv $FA_SUBDIR/* .
 rmdir $FA_SUBDIR
