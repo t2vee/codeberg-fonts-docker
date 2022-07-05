5e1f69ecac6b6f3cc57c9f9761d6fb863249e2b8
n <n@noreply.codeberg.org>
2022-07-05 18:38:03 +0900

    Update deployment script and repo links

--- LICENSE.md
+++ LICENSE.md
@@ -2,7 +2,7 @@ This website by [The Codeberg Fonts Contributors](CONTRIBUTORS.md)
 is licensed under [CC-BY-SA 4.0](https://creativecommons.org/licenses/by-sa/4.0/deed.en).
 
 Changes to the original versions of the works as well as their individual authors can be
-looked up in [the website's source code repository](https://codeberg.org/codeberg-fonts/codeberg-fonts).
+looked up in [the website's source code repository](https://codeberg.org/Codeberg-Infrastructure/codeberg-fonts).
 
 The Codeberg Logo by mray is licensed under [CC-BY 4.0](https://creativecommons.org/licenses/by/4.0/deed.en).
 
--- deploy.sh
+++ deploy.sh
@@ -3,18 +3,16 @@
 # This script is derived from deploy.sh in Codeberg Documentation by The Codeberg Documentation Contributors,
 # under CC-BY-SA 4.0, https://creativecommons.org/licenses/by-sa/4.0/deed.en
 
+DEPLOYMENT_REPO=${1:-git@codeberg.org:Codeberg-Infrastructure/static-deployments}
+DEPLOYMENT_BRANCH=${2:-fonts}
+
 npm run build
-
-
-if [ ! -d pages.git ]; then
-	mkdir pages.git && cd pages.git
-	git init && git remote add origin "git@codeberg.org:codeberg-fonts/pages.git"
-	cd ..
-fi
-
+rm -rf pages.git
+mkdir pages.git
+( cd pages.git && git init -b "$DEPLOYMENT_BRANCH" )
 rsync -av _site/* pages.git/
-
-cd pages.git
-git add -A
-git commit -am "Deployment at $(date -u -Is)"
-git push origin main -f
+echo "$DEPLOYMENT_BRANCH.codeberg.org" > pages.git/.domains
+( cd pages.git && git add -A )				## add all generated files
+( cd pages.git && git commit -m "Deployment at $(date -u -Is)" )	## commit all
+( cd pages.git && git remote add origin "$DEPLOYMENT_REPO" )
+# ( cd pages.git && git push -f origin "$DEPLOYMENT_BRANCH" )		## force-push and rewrite (empty) history
